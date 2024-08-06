7feb02882298e2a1954f465ded9172bc480fe3db
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-08-06 10:15:28 +0300

    Initialize game library array. Create constructor and methods for toggling completion status.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Script Attached');
+// DOM
+
+// Library array
+const gameLibrary = [];
+
+// Game constructor
+function Game(name, hours, completed) {
+	this.name = name;
+	this.hours = hours;
+	this.completed = completed;
+}
+
+// Game prototype methods
+Game.prototype.isCompleted = function () {
+	this.completed = !this.completed;
+};
+
+const vidya = new Game('marvel midnight suns', '', true);
