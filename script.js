1212017ee562fbf9eaf5b8888efae71a38f8f332
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2024-09-01 16:04:31 +0300

    Refactored entire project to use 'class' and IIFE module pattern. Used class syntax instead of prototype. Moved all global variables in their own IIFE module, thus keeping global scope clean and utilize encapsulation and namespace.

--- script.js
+++ script.js
@@ -1,230 +1,261 @@
-// DOM
-const gameLibraryContainer = document.querySelector('.game-library');
-const modalContainer = document.querySelector('.modal-container');
-const closeModalBtn = modalContainer.querySelector('.close');
-const addGameBtn = document.querySelector('#add-game');
-const removeGamesBtn = document.querySelector('#remove-games');
-const addGameForm = document.querySelector('.modal-form');
-const addGameFormBtn = document.querySelector('.modal-form button');
-const filterBarBtn = document.querySelector('#show-filter');
-const filterBar = document.querySelector('.filter-bar');
-const completionFilter = filterBar.querySelector('#filter-completion');
+// Game library class
+class Game {
+	// Class field
+	static id = 0;
 
-// Library array
-const gameLibrary = [];
+	// Static method
+	static assignId() {
+		return ++this.id;
+	}
 
-// Game constructor
-function Game(name, hours, platform, completed) {
-	this.name = name;
-	this.hours = hours;
-	this.platform = platform;
-	this.completed = completed;
-	this.id = Game.assignId();
+	constructor(name, hours, platform, completed) {
+		this.name = name;
+		this.hours = hours;
+		this.platform = platform;
+		this.completed = completed;
+		this.id = Game.assignId();
+	}
+
+	isCompleted() {
+		this.completed = !this.completed;
+	}
 }
 
-// Static property of Game constructor
-Game.id = 0;
+// Database IIFE
+const Database = (() => {
+	// Library array
+	const gameLibrary = [];
 
-// Static method of Game constructor
-Game.assignId = function () {
-	return ++Game.id;
-};
+	const getDatabase = () => gameLibrary;
 
-// Game prototype methods
-Game.prototype.isCompleted = function () {
-	this.completed = !this.completed;
-};
+	const addToDataBase = (object) => gameLibrary.push(object);
 
-// Open & Close modal
-function openModal() {
-	modalContainer.showModal();
-}
+	const removeFromDatabase = (index) => {
+		gameLibrary.splice(index, 1);
+	};
 
-function closeModal() {
-	modalContainer.close();
-	removeValidationClass();
-	resetFormInputs();
-}
+	const removeAllFromDatabase = () => {
+		gameLibrary.splice(0, gameLibrary.length);
+	};
 
-function toggleFilterBar() {
-	filterBar.classList.toggle('hidden');
-}
+	return {
+		getDatabase,
+		addToDataBase,
+		removeFromDatabase,
+		removeAllFromDatabase,
+	};
+})();
 
-// Add & Remove validation classes
-function addValidationClass() {
-	const requiredInputs = document.querySelectorAll(
-		'.modal-input:not(.checkbox) > *:nth-child(2)',
-	);
-	requiredInputs.forEach((input) => input.classList.add('validate'));
-}
+// GUI IIFE
+const displayController = (() => {
+	// DOM
+	const gameLibraryContainer = document.querySelector('.game-library');
+	const modalContainer = document.querySelector('.modal-container');
+	const closeModalBtn = modalContainer.querySelector('.close');
+	const addGameBtn = document.querySelector('#add-game');
+	const removeGamesBtn = document.querySelector('#remove-games');
+	const addGameForm = document.querySelector('.modal-form');
+	const addGameFormBtn = document.querySelector('.modal-form button');
+	const filterBarBtn = document.querySelector('#show-filter');
+	const filterBar = document.querySelector('.filter-bar');
+	const completionFilter = filterBar.querySelector('#filter-completion');
 
-function removeValidationClass() {
-	const requiredInputs = document.querySelectorAll(
-		'.modal-input:not(.checkbox) > *:nth-child(2)',
-	);
-	requiredInputs.forEach((input) => input.classList.remove('validate'));
-}
+	// Modal
+	const openModal = () => {
+		modalContainer.showModal();
+	};
 
-// Clean inputs
-function resetFormInputs() {
-	addGameForm.reset();
-}
+	const closeModal = () => {
+		modalContainer.close();
+		removeValidationClass();
+		resetFormInputs();
+	};
 
-function getUserInputs() {
-	const gameName = addGameForm.querySelector('#game-name').value;
-	const gameHours = addGameForm.querySelector('#game-hours').value || 0;
-	const gamePlatform = addGameForm.querySelector('#game-platform').value;
-	const gameCompleted = addGameForm.querySelector('#game-completion').checked;
+	// Add & Remove validation classes
+	const addValidationClass = () => {
+		const requiredInputs = document.querySelectorAll(
+			'.modal-input:not(.checkbox) > *:nth-child(2)',
+		);
+		requiredInputs.forEach((input) => input.classList.add('validate'));
+	};
 
-	return { gameName, gameHours, gamePlatform, gameCompleted };
-}
+	const removeValidationClass = () => {
+		const requiredInputs = document.querySelectorAll(
+			'.modal-input:not(.checkbox) > *:nth-child(2)',
+		);
+		requiredInputs.forEach((input) => input.classList.remove('validate'));
+	};
 
-function renderNewCard(object) {
-	const card = document.createElement('article');
-	card.className = 'card';
+	const toggleFilterBar = () => {
+		filterBar.classList.toggle('hidden');
+	};
 
-	const cardTitle = document.createElement('div');
-	const platform = document.createElement('h3');
-	const platformIcon = document.createElement('span');
-	const removeBtn = document.createElement('span');
-	const cardBody = document.createElement('div');
-	const gameTitle = document.createElement('h4');
-	const gameHoursContainer = document.createElement('p');
-	const gameHours = document.createElement('span');
-	const completionContainer = document.createElement('div');
-	const completionLabel = document.createElement('label');
-	const completionCheckbox = document.createElement('input');
+	// Clean inputs
+	const resetFormInputs = () => {
+		addGameForm.reset();
+	};
 
-	cardTitle.className = 'card-title';
-	platform.textContent = `${object.platform} Game`;
-	platformIcon.className = object.platform.toLowerCase();
-	removeBtn.className = 'close';
-	cardBody.className = 'card-body';
-	gameTitle.id = 'game-title';
-	gameTitle.textContent = object.name;
-	gameHoursContainer.textContent = 'Hours Played ';
-	gameHours.id = 'game-hours';
-	gameHours.textContent = `${object.hours}H`;
-	completionContainer.className = 'checkbox-container';
-	completionLabel.htmlFor = `game-status-${object.id}`;
-	completionLabel.textContent = 'Completed:';
-	completionCheckbox.name = `game-status-${object.id}`;
-	completionCheckbox.type = 'checkbox';
-	completionCheckbox.id = `game-status-${object.id}`;
-	completionCheckbox.checked = object.completed;
+	const getUserInputs = () => {
+		const gameName = addGameForm.querySelector('#game-name').value;
+		const gameHours = addGameForm.querySelector('#game-hours').value || 0;
+		const gamePlatform = addGameForm.querySelector('#game-platform').value;
+		const gameCompleted = addGameForm.querySelector('#game-completion').checked;
 
-	platform.prepend(platformIcon);
-	cardTitle.append(platform, removeBtn);
-	gameHoursContainer.appendChild(gameHours);
-	completionContainer.append(completionLabel, completionCheckbox);
-	cardBody.append(gameTitle, gameHoursContainer, completionContainer);
+		return { gameName, gameHours, gamePlatform, gameCompleted };
+	};
 
-	// Remove element
-	removeBtn.addEventListener('click', () => {
-		card.remove();
-		removeGameFromLibrary(object.id);
-	});
+	const renderNewCard = (object) => {
+		const card = document.createElement('article');
+		card.className = 'card';
 
-	// Change completions status
-	completionCheckbox.addEventListener('click', () => {
-		object.isCompleted();
+		const cardTitle = document.createElement('div');
+		const platform = document.createElement('h3');
+		const platformIcon = document.createElement('span');
+		const removeBtn = document.createElement('span');
+		const cardBody = document.createElement('div');
+		const gameTitle = document.createElement('h4');
+		const gameHoursContainer = document.createElement('p');
+		const gameHours = document.createElement('span');
+		const completionContainer = document.createElement('div');
+		const completionLabel = document.createElement('label');
+		const completionCheckbox = document.createElement('input');
+
+		cardTitle.className = 'card-title';
+		platform.textContent = `${object.platform} Game`;
+		platformIcon.className = object.platform.toLowerCase();
+		removeBtn.className = 'close';
+		cardBody.className = 'card-body';
+		gameTitle.id = 'game-title';
+		gameTitle.textContent = object.name;
+		gameHoursContainer.textContent = 'Hours Played ';
+		gameHours.id = 'game-hours';
+		gameHours.textContent = `${object.hours}H`;
+		completionContainer.className = 'checkbox-container';
+		completionLabel.htmlFor = `game-status-${object.id}`;
+		completionLabel.textContent = 'Completed:';
+		completionCheckbox.name = `game-status-${object.id}`;
+		completionCheckbox.type = 'checkbox';
+		completionCheckbox.id = `game-status-${object.id}`;
+		completionCheckbox.checked = object.completed;
+
+		platform.prepend(platformIcon);
+		cardTitle.append(platform, removeBtn);
+		gameHoursContainer.appendChild(gameHours);
+		completionContainer.append(completionLabel, completionCheckbox);
+		cardBody.append(gameTitle, gameHoursContainer, completionContainer);
+
+		// Remove element
+		removeBtn.addEventListener('click', () => {
+			card.remove();
+			removeGameFromLibrary(object.id);
+		});
+
+		// Change completions status
+		completionCheckbox.addEventListener('click', () => {
+			object.isCompleted();
+			displayStats();
+		});
+
+		card.append(cardTitle, cardBody);
+
+		gameLibraryContainer.appendChild(card);
+	};
+
+	const addGameToLibrary = (event) => {
+		// Prevent default form submission
+		event.preventDefault();
+		// Get user inputs
+		const { gameName, gameHours, gamePlatform, gameCompleted } =
+			getUserInputs();
+		// Create new object
+		const newGame = new Game(gameName, gameHours, gamePlatform, gameCompleted);
+		// Add new game to the Database
+		Database.addToDataBase(newGame);
+		// Render object to the screen
+		renderNewCard(newGame);
+		// Display stats on filter bar
 		displayStats();
-	});
+		// Close modal on form submit
+		closeModal();
+	};
 
-	card.append(cardTitle, cardBody);
+	const removeGameFromLibrary = (id) => {
+		const database = Database.getDatabase();
+		const gameIndex = database.findIndex((game) => game.id === id);
+		Database.removeFromDatabase(gameIndex);
+		displayStats();
+	};
 
-	gameLibraryContainer.appendChild(card);
-}
+	const removeAllGamesFromLibrary = () => {
+		Database.removeAllFromDatabase();
+		gameLibraryContainer.innerText = '';
+		displayStats();
+	};
 
-function addGameToLibrary(event) {
-	// Prevent default form submission
-	event.preventDefault();
-	// Get user inputs
-	const { gameName, gameHours, gamePlatform, gameCompleted } = getUserInputs();
-	// Create new object
-	const newGame = new Game(gameName, gameHours, gamePlatform, gameCompleted);
-	// Add object ot the library array
-	gameLibrary.push(newGame);
-	// Render object to the screen
-	renderNewCard(newGame);
-	// Display stats on filter bar
-	displayStats();
-	// Close modal on form submit
-	closeModal();
-}
+	const displayStats = () => {
+		const totalGamesAddedEl = filterBar.querySelector('#total-added');
+		const totalGamesCurrentEl = filterBar.querySelector('#total-current');
+		const completedGamesEl = filterBar.querySelector('#games-completed');
+		const notCompletedGamesEl = filterBar.querySelector('#games-not-completed');
 
-function removeGameFromLibrary(id) {
-	const gameIndex = gameLibrary.findIndex((game) => game.id === id);
-	gameLibrary.splice(gameIndex, 1);
-	displayStats();
-}
+		const database = Database.getDatabase();
+		const totalGames = Game.id;
+		const totalGamesCurrent = database.length;
+		const completedGames = database.filter(
+			(game) => game.completed === true,
+		).length;
+		const notCompletedGames = database.filter(
+			(game) => game.completed === false,
+		).length;
 
-function removeAllGamesFromLibrary() {
-	gameLibrary.splice(0, gameLibrary.length);
-	gameLibraryContainer.innerText = '';
-	displayStats();
-}
+		totalGamesAddedEl.textContent = totalGames;
+		totalGamesCurrentEl.textContent = totalGamesCurrent;
+		completedGamesEl.textContent = completedGames;
+		notCompletedGamesEl.textContent = notCompletedGames;
+	};
 
-function displayStats() {
-	const totalGamesAddedEl = filterBar.querySelector('#total-added');
-	const totalGamesCurrentEl = filterBar.querySelector('#total-current');
-	const completedGamesEl = filterBar.querySelector('#games-completed');
-	const notCompletedGamesEl = filterBar.querySelector('#games-not-completed');
+	const filterGamesByStatus = (status) => {
+		const cards = document.querySelectorAll('.card');
 
-	const totalGames = Game.id;
-	const totalGamesCurrent = gameLibrary.length;
-	const completedGames = gameLibrary.filter(
-		(game) => game.completed === true,
-	).length;
-	const notCompletedGames = gameLibrary.filter(
-		(game) => game.completed === false,
-	).length;
+		cards.forEach((card) => {
+			const gameStatus = card.querySelector("input[type='checkbox']").checked;
+			// Hide card
+			card.style.display = 'none';
+			// Display card on a condition
+			if (status === 'all') {
+				card.style.display = 'grid';
+			}
 
-	totalGamesAddedEl.textContent = totalGames;
-	totalGamesCurrentEl.textContent = totalGamesCurrent;
-	completedGamesEl.textContent = completedGames;
-	notCompletedGamesEl.textContent = notCompletedGames;
-}
+			if (status === 'completed' && gameStatus) {
+				card.style.display = 'grid';
+			}
 
-function filterGamesByStatus(status) {
-	const cards = document.querySelectorAll('.card');
+			if (status === 'not-completed' && !gameStatus) {
+				card.style.display = 'grid';
+			}
+		});
+	};
 
-	cards.forEach((card) => {
-		const gameStatus = card.querySelector("input[type='checkbox']").checked;
-		// Hide card
-		card.style.display = 'none';
-		// Display card on a condition
-		if (status === 'all') {
-			card.style.display = 'grid';
-		}
+	const attachEventHandlers = () => {
+		// Event listeners
+		addGameBtn.addEventListener('click', openModal);
 
-		if (status === 'completed' && gameStatus) {
-			card.style.display = 'grid';
-		}
+		closeModalBtn.addEventListener('click', closeModal);
 
-		if (status === 'not-completed' && !gameStatus) {
-			card.style.display = 'grid';
-		}
-	});
-}
-// Event listeners
-addGameBtn.addEventListener('click', openModal);
+		addGameFormBtn.addEventListener('click', () => {
+			addValidationClass();
+		});
 
-closeModalBtn.addEventListener('click', closeModal);
+		addGameForm.addEventListener('submit', addGameToLibrary);
 
-addGameFormBtn.addEventListener('click', () => {
-	addValidationClass();
-});
+		removeGamesBtn.addEventListener('click', removeAllGamesFromLibrary);
 
-addGameForm.addEventListener('submit', addGameToLibrary);
+		filterBarBtn.addEventListener('click', toggleFilterBar);
 
-removeGamesBtn.addEventListener('click', removeAllGamesFromLibrary);
-
-filterBarBtn.addEventListener('click', toggleFilterBar);
-
-completionFilter.addEventListener('change', (e) => {
-	const filter = e.target.value;
-	filterGamesByStatus(filter);
-});
-
-displayStats();
+		completionFilter.addEventListener('change', (e) => {
+			const filter = e.target.value;
+			filterGamesByStatus(filter);
+		});
+	};
+	attachEventHandlers();
+})();
